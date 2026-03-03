f67227c1e905fd0e3f0fb5191b3c84f8e6530003
DieSpurMann <diespurmann@gmail.com>
2026-03-03 14:36:41 +0100

    [INIT] I've generated a new component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TeamComponent } from './team-component/team-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TeamComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
