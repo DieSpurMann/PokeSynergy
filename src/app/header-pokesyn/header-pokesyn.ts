77924024fbdb8ca02543ba73bd1d25d44e6483f6
DieSpurMann <diespurmann@gmail.com>
2026-03-06 15:46:30 +0100

    [Added] Menu Burger

--- src/app/header-pokesyn/header-pokesyn.ts
+++ src/app/header-pokesyn/header-pokesyn.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header-pokesyn',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header-pokesyn.html',
   styleUrl: './header-pokesyn.scss',
 })
 export class HeaderPokesyn {
-
+  isMenuOpen: boolean = false;
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+    console.log('Le menu est ouvert ?', this.isMenuOpen);
+  }
 }
