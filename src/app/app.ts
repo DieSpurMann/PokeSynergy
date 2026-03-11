380783ca66ab81cb06d41d64263dc180a08dcdbc
cyriaque <cyriaque.barrault@gmail.com>
2026-03-11 10:06:51 +0100

    Page de connexion finale

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { TeamComponent } from './team-component/team-component';
 import { HeaderPokesyn } from './header-pokesyn/header-pokesyn';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from "./login-component/login-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TeamComponent, HeaderPokesyn, CommonModule],
+  imports: [RouterOutlet, TeamComponent, HeaderPokesyn, CommonModule, LoginComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
