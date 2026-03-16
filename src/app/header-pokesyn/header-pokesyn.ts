a1fbe0a0b861ca38ba11b29d44337ebc6bde8d11
cyriaque <cyriaque.barrault@gmail.com>
2026-03-16 09:08:10 +0100

    Je sais plus, je crois que c'était des modif sur la position de la PP

--- src/app/header-pokesyn/header-pokesyn.ts
+++ src/app/header-pokesyn/header-pokesyn.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header-pokesyn',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [
+    CommonModule, 
+    RouterModule
+  ],
   templateUrl: './header-pokesyn.html',
   styleUrl: './header-pokesyn.scss',
 })
