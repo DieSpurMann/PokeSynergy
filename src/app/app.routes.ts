504a9ced71e6d4023208570216a97a71811479e7
cyriaque <cyriaque.barrault@gmail.com>
2026-03-16 09:54:09 +0100

    La mage 404 s'affiche quand l'URL n'est pas correct et les boutons marchent.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Error404Component } from './error-404-component/error-404-component';
+import { LoginComponent } from './login-component/login-component';
+import { HeaderPokesyn } from './header-pokesyn/header-pokesyn';
+import { Pokedex } from './pokedex/pokedex'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Redirection par défaut (si l'utilisateur arrive sur la racine '')
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  //
+  { path: 'pokedex', component: Pokedex },
+
+  // Route pour la page de connexion
+  { path: 'login', component: LoginComponent },
+
+  // LA ROUTE 404 : doit TOUJOURS être la dernière du tableau
+  { path: '**', component: Error404Component }
+];
